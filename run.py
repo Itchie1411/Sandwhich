090165f022e260d6606ee44ab5edafc19ba6f733
Itchie1411 <richardglennon97@gmail.com>
2022-03-29 20:27:55 +0000

    creating 2 functions to calculate if the data entered is 6 characters long. Also a new function created asking for a data input from the user.

--- run.py
+++ run.py
@@ -2,7 +2,6 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 
-
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -15,6 +14,34 @@ GSPREAD_CLIENT = gspread.authorize(SCOPTED_CREDS)
 SHEET = GSPREAD_CLIENT.open('sandwiches')
 
 
-sales = SHEET.worksheet('sales')
-data = sales.get_all_values()
-print(data)
+def get_sales_data():
+    """
+    get sales figures"""
+
+    print('please enter sales data from last market')
+    print('data should be siz nunmbers, seperated by comma')
+    print('example: 20, 15, 16, ... etc\n')
+
+    data_str = input('Enter data here: ')
+
+    sales_data = data_str.split(',')
+
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    try:
+       if len(values) != 6:
+           raise ValueError(
+               f'Exactly 6 values required, you provided {len(values)}'
+           )
+    except ValueError as e:
+        print(f'invalid data {e}, please try again')
+
+
+
+
+
+
+
+get_sales_data()
